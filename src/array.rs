b7962cdcd3b7beec39be7a3c3eef61aa8c36082d
LangArthur <arthur.lang.pro@gmail.com>
2026-06-15 08:11:40 +0200

    feat: add random shuffle

--- src/array.rs
+++ src/array.rs
@@ -17,12 +17,25 @@ where
     prefix
 }
 
+// Shuffle a vector using random integers
+pub fn random_shuffle<T>(arr: &mut [T]) -> &[T]
+where
+    T: PartialEq {
+    let mut n = arr.len();
+    while n > 1 {
+        let new_idx = rand::random_range(0..n);
+        n -= 1;
+        arr.swap(n, new_idx);
+    }
+    arr
+} 
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_int() {
+    fn test_prefix_sum_int() {
         assert_eq!(
             Vec::<i32>::from([0, 1, 3, 8, 12]),
             prefix_sums(&[1, 2, 5, 4])
@@ -39,15 +52,32 @@ mod tests {
     }
 
     #[test]
-    fn test_empty() {
+    fn test_prefix_sum_empty() {
         assert_eq!(Vec::<i32>::from([]), prefix_sums(&[]))
     }
 
     #[test]
-    fn test_float() {
+    fn test_prefix_sum_float() {
         assert_eq!(
             Vec::<f32>::from([0f32, 1f32, 3.3f32, 8.5f32, 12.5f32]),
             prefix_sums(&[1f32, 2.3f32, 5.2f32, 4f32])
+        )
+    }
+
+    #[test]
+    fn test_random_shuffle() {
+        let mut vec = vec![0f32, 1f32, 3.3f32, 8.5f32, 12.5f32];
+        assert_ne!(
+            vec.clone(),
+            random_shuffle(&mut vec)
+        )
+    }
+    #[test]
+    fn test_random_shuffle_empty() {
+        let mut empty: Vec<i8> = vec![];
+        assert_eq!(
+            empty.clone(),
+            random_shuffle(&mut empty)
         )
     }
 }
